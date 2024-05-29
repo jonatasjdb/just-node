1c0c1a42af061951f787f2bf5e2be1d207a78c98
Jônatas Dias <jonatasjdb@hotmail.com>
2024-05-28 23:34:16 -0300

    Studying Streams concept

--- src/server.js
+++ src/server.js
@@ -1,6 +1,30 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((req, res) => {
+    const {method, url} = req
+
+    if(method === 'GET' && url === '/users') {
+        return res
+        .setHeader('Content-type','application/json')
+        .end(JSON.stringify(users))
+    }
+
+    if(method === 'POST' && url === '/users'){
+        users.push({
+            id: 1,
+            name: 'JD',
+            email: 'jd@gmail.com'
+        })
+
+        return res.writeHead(201).end()
+    }
+
+    return res.writeHead(404).end('Not Found Route')
+
+
+
     return res.end('Hello!')
 })
 
